6bb7de04ff468b9b681bca93685ace9b223c9e5d
Adam Nickle <anickle060193@gmail.com>
2019-08-17 07:45:57 -0500

    Initial release.

---
+++ assets/export_icons.bat
@@ -0,0 +1,1 @@
+inkscape --file=ic_launcher.svg --export-png=promotional_icon.png --export-area-page --export-width=512 --export-height=512
---
+++ assets/export_promo.bat
@@ -0,0 +1,1 @@
+inkscape --file=promotional_graphic.svg --export-png=promotional_graphic.png --export-area-page --export-width=1024 --export-height=500
